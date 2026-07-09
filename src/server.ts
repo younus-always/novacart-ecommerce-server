27d9e3ced62ce1fbee037bc72fa797eab58cecd9
younus-always <younusofficial443@gmail.com>
2026-07-09 18:24:34 +0600

    error handler for server = unhandledRejection, uncaughtException, SIGINT, SIGTERM

--- src/server.ts
+++ src/server.ts
@@ -17,3 +17,63 @@ const startServer = async () => {
       }
 };
 startServer();
+
+
+
+/**
+ * unhandledRejection
+ * Promise.reject(new Error("I forgot to handle promise error"))
+ * 
+ * uncaughtException
+ * throw new Error("I forgot to handle local error")
+ */
+
+
+process.on("unhandledRejection", () => {
+      console.log("Unhandled Rejection Detected. Server Shutting Down....");
+
+      if (server) {
+            server.close(() => {
+                  process.exit(1);
+            });
+      };
+
+      process.exit(1);
+});
+
+process.on("uncaughtException", () => {
+      console.log("Uncaught Exception Detected. Server Shutting Down....");
+
+      if (server) {
+            server.close(() => {
+                  process.exit(1);
+            });
+      };
+
+      process.exit(1);
+});
+
+
+process.on("SIGINT", () => {
+      console.log("SIGINT Signal Received. Server Shutting Down....");
+
+      if (server) {
+            server.close(() => {
+                  process.exit(1);
+            });
+      };
+
+      process.exit(1);
+});
+
+process.on("SIGTERM", () => {
+      console.log("SIGTERM Signal Received. Server Shutting Down....");
+
+      if (server) {
+            server.close(() => {
+                  process.exit(1);
+            });
+      };
+
+      process.exit(1);
+});
