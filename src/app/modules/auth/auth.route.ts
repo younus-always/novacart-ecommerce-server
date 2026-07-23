0c773bb40f963732f3a12796ef24dd80325b9a21
younus-always <younusofficial443@gmail.com>
2026-07-23 21:36:23 +0600

    auth: generate-new-accessToken controller and set-cookie to accessToken

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -5,3 +5,4 @@ export const authRoutes = Router();
 
 authRoutes.post("/login", authController.loginUser);
 authRoutes.post("/logout", authController.logoutUser);
+authRoutes.post("/refresh-token", authController.getNewAccessToken);
