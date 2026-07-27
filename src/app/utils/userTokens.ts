30529287457e833194496e3d15b781eb96d9f758
younus-always <younusofficial443@gmail.com>
2026-07-27 18:38:57 +0600

    modified getNewAccessToken service function to create new function createNewAccessTokenByRefreshToken

--- src/app/utils/userTokens.ts
+++ src/app/utils/userTokens.ts
@@ -1,6 +1,10 @@
 import { envVars } from "../config/env";
-import { IUser } from "../modules/user/user.interface";
-import { generateToken } from "./jwt";
+import { AppError } from "../Error/AppError";
+import { IsActive, IUser } from "../modules/user/user.interface";
+import { User } from "../modules/user/user.model";
+import { generateToken, verifyToken } from "./jwt";
+import httpStatus from 'http-status-codes';
+
 
 export const createUserTokens = (user: Partial<IUser>) => {
       const jwtPayload = {
@@ -16,4 +20,33 @@ export const createUserTokens = (user: Partial<IUser>) => {
             accessToken,
             refreshToken
       };
+};
+
+
+export const createNewAccessTokenByRefreshToken = async (refreshToken: string) => {
+      const verifiedRefreshToken = verifyToken(refreshToken, envVars.JWT.REFRESH_SECRET_TOKEN);
+
+      const user = await User.findOne({ email: verifiedRefreshToken.email });
+
+      if (!user) {
+            throw new AppError(httpStatus.NOT_FOUND, "User not found!")
+      };
+
+      if (user.isActive === IsActive.INACTIVE || user.isActive === IsActive.BLOCKED) {
+            throw new AppError(httpStatus.FORBIDDEN, `User account is ${user.isActive}`);
+      };
+
+      if (user.isDeleted) {
+            throw new AppError(httpStatus.FORBIDDEN, "User account is deleted!")
+      };
+
+      const jwtPayload = {
+            userId: user._id,
+            email: user.email,
+            role: user.role
+      };
+
+      const accessToken = generateToken(jwtPayload, envVars.JWT.ACCESS_SECRET_TOKEN, envVars.JWT.ACCESS_TOKEN_EXPIRES);
+
+      return accessToken;
 };
